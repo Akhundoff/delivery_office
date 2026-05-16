606a25fa4744f832fc142791a28932ec9d57e093
Farid <faridshadmanli@gmail.com>
2026-05-16 13:20:00 +0400

    feat: add filterOptionWithNameOrId and filterOptionStart antd select strategies

--- src/shared/modules/antd/helpers/filter-option.ts
+++ src/shared/modules/antd/helpers/filter-option.ts
@@ -1,8 +1,31 @@
 export const filterOption = (input: string, option: any) => {
-    if (typeof option?.children === 'string') {
-        return option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0;
-    } else if (Array.isArray(option?.children)) {
-        return option.children.join('').toLowerCase().indexOf(input.toLowerCase()) >= 0;
-    }
-    return false;
+  if (typeof option?.children === 'string') {
+    return option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0;
+  } else if (Array.isArray(option?.children)) {
+    return option.children.join('').toLowerCase().indexOf(input.toLowerCase()) >= 0;
+  }
+  return false;
+};
+
+export const filterOptionWithNameOrId = (input: string, option: any) => {
+  const parsedInput = parseInt(input.toLowerCase());
+  if (typeof option?.children === 'string') {
+    return Number.isNaN(parsedInput)
+      ? option.children.toLowerCase().split(' ')[3]?.includes(input.toLowerCase()) ?? false
+      : option.children.toLowerCase().split(' ')[1]?.includes(parsedInput.toString()) ?? false;
+  } else if (Array.isArray(option?.children)) {
+    return Number.isNaN(parsedInput)
+      ? option.children[option.children.length - 1]?.toLowerCase().includes(input.toLowerCase()) ?? false
+      : option.children[1]?.toString().toLowerCase().startsWith(parsedInput.toString()) ?? false;
+  }
+  return false;
+};
+
+export const filterOptionStart = (input: string, option: any) => {
+  if (typeof option?.children === 'string') {
+    return option.children.toLowerCase().indexOf(input.toLowerCase()) === 1;
+  } else if (Array.isArray(option?.children)) {
+    return option.children.join('').toLowerCase().indexOf(input.toLowerCase()) === 1;
+  }
+  return false;
 };
