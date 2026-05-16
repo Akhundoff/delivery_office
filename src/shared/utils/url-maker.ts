675ff7eb78032be2ce8a18fdacdc4f96e1fd95bb
Farid <faridshadmanli@gmail.com>
2026-05-16 13:17:40 +0400

    feat: add localURLMaker and staticFileUrl to url-maker utils

--- src/shared/utils/url-maker.ts
+++ src/shared/utils/url-maker.ts
@@ -5,3 +5,16 @@ export const urlMaker = (pathname: string, params: Record<string, unknown> = {})
   const queryParams: string = object2Search(params);
   return host + pathname + queryParams;
 };
+
+export const localURLMaker = (pathname: string, params: Record<string, string | number> = {}, query: Record<string, any> = {}) => {
+  const filledPathname = Object.entries(params)
+    .filter(([, value]) => !!value)
+    .reduce((acc, [key, value]) => acc.replace(':' + key, value.toString()), pathname);
+
+  const queryString = object2Search(query);
+  return filledPathname + queryString;
+};
+
+export const staticFileUrl = (fileName: string): string => {
+  return urlMaker(localURLMaker('/storage/files/:fileName', { fileName }));
+};
