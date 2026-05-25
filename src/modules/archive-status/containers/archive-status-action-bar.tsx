7495e8fcf40288ddcc96e5b9899ca05734db34fc
Farid <faridshadmanli@gmail.com>
2026-05-25 21:49:21 +0400

    feat(archive-status): add select-all and reset-selection buttons

--- src/modules/archive-status/containers/archive-status-action-bar.tsx
+++ src/modules/archive-status/containers/archive-status-action-bar.tsx
@@ -1,18 +1,28 @@
-import { useContext } from "react";
-import { Space } from "antd";
-import * as Icons from "@ant-design/icons";
-import { HeadPortal } from "@modules/layout/components/head-portal";
-import { StyledHeaderButton } from "@modules/layout/styled";
-import { StyledActionBar } from "@shared/styled/action-bar";
-import { ArchiveStatusTableContext } from "../context";
+import { useContext } from 'react';
+import { Space } from 'antd';
+import * as Icons from '@ant-design/icons';
+import { HeadPortal } from '@modules/layout/components/head-portal';
+import { StyledHeaderButton } from '@modules/layout/styled';
+import { StyledActionBar } from '@shared/styled/action-bar';
+import { ArchiveStatusTableContext } from '../context';
 
 export const ArchiveStatusActionBar = () => {
-  const { handleFetch, handleReset } = useContext(ArchiveStatusTableContext);
+  const { handleFetch, handleReset, handleSelectAll, handleResetSelection, state } = useContext(ArchiveStatusTableContext);
+  const selectionCount = Object.keys(state.selectedRowIds).length;
 
   return (
     <HeadPortal>
       <StyledActionBar $flex={true}>
         <Space>
+          {selectionCount === 0 ? (
+            <StyledHeaderButton type="text" onClick={handleSelectAll} icon={<Icons.CheckCircleOutlined />}>
+              Hamısını seç
+            </StyledHeaderButton>
+          ) : (
+            <StyledHeaderButton type="text" onClick={handleResetSelection} icon={<Icons.CloseCircleOutlined />}>
+              {selectionCount} sətir seçilib
+            </StyledHeaderButton>
+          )}
           <StyledHeaderButton type="text" onClick={handleFetch} icon={<Icons.ReloadOutlined />}>
             Yenilə
           </StyledHeaderButton>
