34f39ea38cbcc1207ee13c981faf44fe6847c15e
Farid <faridshadmanli@gmail.com>
2026-06-14 11:45:28 +0400

    feat: test

--- src/App.tsx
+++ src/App.tsx
@@ -8,32 +8,33 @@ import { CountryProvider } from './modules/country';
 import { MeProvider } from './modules/me';
 import { NotificationProvider } from './modules/notifications';
 import { SettingsProvider } from './modules/settings';
-import { MainRouter } from './router';import { TableCacheProvider } from './shared/modules/next-table/context/table-cache';
+import { MainRouter } from './router';
+import { TableCacheProvider } from './shared/modules/next-table/context/table-cache';
 
 const queryClient = new QueryClient({ defaultOptions: { queries: { refetchOnWindowFocus: false, retry: false } } });
 
 const App = () => {
-    return (
-        <QueryClientProvider client={queryClient}>
-            <BrowserRouter>
-                <ConfigProvider locale={antLocaleAz}>
-                    <NotificationProvider>
-                        <TableCacheProvider>
-                            <MeProvider>
-                                <SettingsProvider>
-                                    <CountryProvider>
-                                        <CounterProvider>
-                                            <MainRouter />
-                                        </CounterProvider>
-                                    </CountryProvider>
-                                </SettingsProvider>
-                            </MeProvider>
-                        </TableCacheProvider>
-                    </NotificationProvider>
-                </ConfigProvider>
-            </BrowserRouter>
-        </QueryClientProvider>
-    );
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ConfigProvider locale={antLocaleAz}>
+          <NotificationProvider>
+            <TableCacheProvider>
+              <MeProvider>
+                <SettingsProvider>
+                  <CountryProvider>
+                    <CounterProvider>
+                      <MainRouter />
+                    </CounterProvider>
+                  </CountryProvider>
+                </SettingsProvider>
+              </MeProvider>
+            </TableCacheProvider>
+          </NotificationProvider>
+        </ConfigProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 };
 
 export default App;
